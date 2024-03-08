06e323dc5470e647688a0fbd41cd8bb0c9ab0dac
KoiBirb <centralleo474@gmail.com>
2024-03-08 09:35:46 -0500

    working vers

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Creates a java window
+        JFrame window = new JFrame();
+        // Change window settings
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Cellular Automata");
+
+        GamePanel gamePanel = new GamePanel();
+
+
+        // place objects
+        window.add(gamePanel);
+        window.pack();
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        // start game
+        gamePanel.setupGame();
+        gamePanel.startGameThread();
+    }
+}
---
+++ src/Map.java
@@ -0,0 +1,89 @@
+import java.awt.*;
+import java.security.SecureRandom;
+
+public class Map {
+
+    GamePanel gp;
+    SecureRandom random = new SecureRandom();
+    public int[][] grid;
+    int p = 4;
+    public Map (GamePanel gp){
+        this.gp = gp;
+    }
+
+    public void setupMap (){
+        grid = new int[gp.screenHeight][gp.screenWidth];
+
+        for (int y = 0; y < gp.screenHeight; y++) {
+            for (int x = 0; x < gp.screenWidth; x++) {
+                grid[y][x] = random.nextInt(0,2);
+            }
+        }
+    }
+
+    public void update(){
+        int[][] updatedGrid = new int[gp.screenHeight][gp.screenWidth];
+
+        for (int y = 0; y < gp.screenHeight; y++) {
+            for (int x = 0; x < gp.screenWidth; x++) {
+                updatedGrid[y][x] = checkAdjacentTiles(y,x);
+            }
+        }
+        grid = updatedGrid;
+        if (p == 4){
+            p = 3;
+        } else {
+            p = 4;
+        }
+    }
+
+    public int checkAdjacentTiles (int y, int x){
+        int choice = 0, wallCount = 0;
+
+        try {
+            if (grid[y][x - 1] == 1) {
+                wallCount++;
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {}
+
+        try {
+            if (grid[y][x + 1] == 1) {
+                wallCount++;
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {}
+
+        for(int i = 0; i<3; i++){
+            try {
+                if (grid[y - 1][x - 1 + i] == 1) {
+                    wallCount++;
+                }
+            }catch (ArrayIndexOutOfBoundsException e){}
+        }
+
+        for(int i = 0; i<3; i++){
+            try {
+                if (grid[y + 1][x - 1 + i] == 1) {
+                    wallCount++;
+                }
+            }catch (ArrayIndexOutOfBoundsException e){}
+        }
+
+        if (wallCount > p){
+            choice++;
+        }
+        return choice;
+    }
+
+    public void draw (Graphics g2){
+        for (int y = 0; y < gp.gridHeight; y++){
+            for (int x = 0; x < gp.gridWidth; x++){
+                if (grid[y][x] == 1){
+                    g2.setColor(Color.WHITE);
+                } else {
+                    g2.setColor(Color.BLACK);
+                }
+                g2.fillRect(x * gp.tileSize, y * gp.tileSize, gp.tileSize, gp.tileSize);
+            }
+        }
+    }
+}
